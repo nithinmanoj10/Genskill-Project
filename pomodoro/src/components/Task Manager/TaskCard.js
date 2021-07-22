2a33704b4b3a1b6c36f34ab9fb2c250a7d40dbf9
nithinmanoj10 <64126131+nithinmanoj10@users.noreply.github.com>
2021-07-22 20:19:26 +0530

    Router Bug Fix 12

--- pomodoro/src/components/Task Manager/TaskCard.js
+++ pomodoro/src/components/Task Manager/TaskCard.js
@@ -21,24 +21,23 @@ function TaskCard(props) {
     task_isCompleted,
   } = props.task;
 
-  const tagStyles = {};
+  // const tagStyles = {};
+  // console.log(tagStyles);
 
-  if (tagsData.length !== 0) {
-    const task_colour = tagsData.filter(function (tag) {
-      if (tag.name === task_tag) {
-        return tag.colour;
-      }
-    });
-    tagStyles.color = task_colour[0].colour;
-  }
+  // if (tagsData.length !== 0) {
+  //   const task_colour = tagsData.filter(function (tag) {
+  //     if (tag.name === task_tag) {
+  //       return tag.colour;
+  //     }
+  //   });
+  //   tagStyles.color = task_colour[0].colour;
+  // }
 
   // console.log(task_colour[0].colour);
 
   // const tagStyles = {
   //   color: task_colour[0].colour,
   // };
-
-  console.log(tagStyles);
 
   const { tasks, setTasks } = props;
 
@@ -67,9 +66,7 @@ function TaskCard(props) {
       }`}
     >
       <div className="task-info">
-        <p className="task-info__tag" style={tagStyles}>
-          {task_tag}
-        </p>
+        <p className="task-info__tag">{task_tag}</p>
         <h4 className="task-info__name">{task_title}</h4>
         <p className="task-info__desc">{task_desc}</p>
       </div>
